2b0839a1e57a7e88357cc87a9e4b1c62608f8928
Swagat <swagat.khot@westtn.consulting>
2026-04-20 18:34:16 -0400

    Add projectId to firebaseConfig
    Added projectId to firebaseConfig for initialization.

--- src/constants/firebaseConfig.ts
+++ src/constants/firebaseConfig.ts
@@ -3,7 +3,8 @@ import { getDatabase } from 'firebase/database';
 
 const firebaseConfig = {
   apiKey: "AIzaSyA8TsVM19zSqGhKo4L_zMnV8acPXaVl4sA",
-  databaseURL: "https://laundryapp-cd794-default-rtdb.firebaseio.com"
+  databaseURL: "https://laundryapp-cd794-default-rtdb.firebaseio.com",
+  projectId: "laundryapp-cd794"
 };
 
 const app = initializeApp(firebaseConfig);
